ea87267dfe73c859d62f32067f4797192c8657de
isac <cortesbuitragoisac@gmail.com>
2025-05-02 20:13:45 +0000

    Create backend config script

---
+++ deployScripts/backendSetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ~/Geocitizen/
+
+#change pom.xml config
+sed -i \
+  -e '/<repositories>/,/<\/repositories>/c\
+<repository>\
+    <id>central</id>\
+    <url>https://repo.maven.apache.org/maven2</url>\
+    <snapshots>\
+        <enabled>false</enabled>\
+    </snapshots>\
+</repository>' \
+  -e '/<distributionManagement>/,/<\/distributionManagement>/d' \
+  -e 's/<artifactId>servlet-api<\/artifactId>/<artifactId>javax.servlet-api<\/artifactId>/' \
+  -e 's/<springframework\.social\.facebook\.version>3\.0\.0\.M3<\/springframework\.social\.facebook\.version>/<springframework.social.facebook.version>2.0.3.RELEASE<\/springframework.social.facebook.version>/' \
+  pom.xml
+
+#change application.properties config
+sed -i \
+  -e "s|^front\.url=http://localhost:8080/citizen/#|front.url=http://geocitizen.com:8080/citizen/#|" \
+  -e "s|^front-end\.url=http://localhost:8080/citizen/|front-end.url=http://geocitizen.com:8080/citizen/|" \
+  -e "s|^email\.username=ssgeocitizen@gmail.com|email.username=cortesbuitragoisac@gmail.com|" \
+  -e "s|^email\.password=softserve|email.password=ylsh ajnb hzaq ildt|" \
+  -e "s|^google\.appKey=.*|google.appKey=740741452799-9tg0v0kjjlnvgb5f311pc5c81t48as68.apps.googleusercontent.com|" \
+  -e "s|^google\.appSecret=.*|google.appSecret=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|" \
+  src/main/resources/application.properties
+
